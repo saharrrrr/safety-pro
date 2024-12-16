e583b984dc5981e385e4d48432049c83064e43de
saharrrrr <skmao23@gmail.com>
2024-12-16 19:38:40 +0300

    update cardSlider

--- safety-pro/src/components/CardSlider/CardSlider.jsx
+++ safety-pro/src/components/CardSlider/CardSlider.jsx
@@ -6,44 +6,58 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar";
 
+import service1 from "../../assets/service1.png"
+import service2 from "../../assets/service2.png"
+import service3 from "../../assets/service3.png"
+import service4 from "../../assets/service4.png"
+import service5 from "../../assets/service5.png"
+import service6 from "../../assets/service6.png"
+import service7 from "../../assets/service7.png"
+
 
 function CardSlider() {
     const cards = [
         {
             id:1,
-            title: "",
-            description:"",
-            img: ""
+            title: "Fire Safety System Design",
+            description:"Design and implement customized firefighting, fire alarm systems, and evacuation plans to ensure full safety compliance.",
+            img: service1
         },
         {
             id:2,
-            title: "",
-            description:"",
-            img: ""
+            title: "Firefighting System Installation",
+            description:"Supply and install water, foam, and fire suppression systems tailored to your safety needs",
+            img: service2
         },
         {
             id:3,
-            title: "",
-            description:"",
-            img: ""
+            title: "Advanced Fire Detection Systems",
+            description:"Install addressable and conventional fire alarm devices, including smoke, heat, flame, and gas detectors.",
+            img: service3
         },
         {
             id:4,
-            title: "",
-            description:"",
-            img: ""
+            title: "Emergency Evacuation Systems",
+            description:"Equip buildings with audio evacuation systems, fire-resistant walls, emergency doors, escape ladders, and assembly points.",
+            img: service4
         },
         {
             id:5,
-            title: "",
-            description:"",
-            img: ""
+            title: "Comprehensive Maintenance Services",
+            description:"Ensure system reliability with routine and emergency maintenance for fire safety and prevention systems.",
+            img: service5
         },
         {
             id:6,
-            title: "",
-            description:"",
-            img: ""
+            title: "Fire Safety Training",
+            description:"Train clients on effective use of fire protection equipment and safety preventive measures.",
+            img: service6
+        },
+        {
+            id:7,
+            title: "Fire Safety Consultations",
+            description:"Provide expert consultations to optimize safety and fire protection systems for your facilities.",
+            img: service7
         },
 
     ];
