f7325ded115167c716669787a3e3f38c92523f4e
saharrrrr <skmao23@gmail.com>
2024-12-16 22:52:18 +0300

    update components

--- safety-pro/src/components/CardSlider/CardSlider.jsx
+++ safety-pro/src/components/CardSlider/CardSlider.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
 import "swiper/css";
@@ -5,96 +6,96 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar";
 
-import "./CardSlider.css"
+import "./CardSlider.css";
 
-import service1 from "../../assets/service1.png"
-import service2 from "../../assets/service2.png"
-import service3 from "../../assets/service3.png"
-import service4 from "../../assets/service4.png"
-import service5 from "../../assets/service5.png"
-import service6 from "../../assets/service6.png"
-import service7 from "../../assets/service7.png"
-
+import service1 from "../../assets/service1.png";
+import service2 from "../../assets/service2.png";
+import service3 from "../../assets/service3.png";
+import service4 from "../../assets/service4.png";
+import service5 from "../../assets/service5.png";
+import service6 from "../../assets/service6.png";
+import service7 from "../../assets/service7.png";
 
 const CardSlider = () => {
-    const cards = [
-        {
-            id:1,
-            title: "Fire Safety System Design",
-            description:"Design and implement customized firefighting, fire alarm systems, and evacuation plans to ensure full safety compliance.",
-            img: service1
-        },
-        {
-            id:2,
-            title: "Firefighting System Installation",
-            description:"Supply and install water, foam, and fire suppression systems tailored to your safety needs",
-            img: service2
-        },
-        {
-            id:3,
-            title: "Advanced Fire Detection Systems",
-            description:"Install addressable and conventional fire alarm devices, including smoke, heat, flame, and gas detectors.",
-            img: service3
-        },
-        {
-            id:4,
-            title: "Emergency Evacuation Systems",
-            description:"Equip buildings with audio evacuation systems, fire-resistant walls, emergency doors, escape ladders, and assembly points.",
-            img: service4
-        },
-        {
-            id:5,
-            title: "Comprehensive Maintenance Services",
-            description:"Ensure system reliability with routine and emergency maintenance for fire safety and prevention systems.",
-            img: service5
-        },
-        {
-            id:6,
-            title: "Fire Safety Training",
-            description:"Train clients on effective use of fire protection equipment and safety preventive measures.",
-            img: service6
-        },
-        {
-            id:7,
-            title: "Fire Safety Consultations",
-            description:"Provide expert consultations to optimize safety and fire protection systems for your facilities.",
-            img: service7
-        },
-
-    ];
+  const cards = [
+    {
+      id: 1,
+      title: "Fire Safety System Design",
+      description:
+        "Design and implement customized firefighting, fire alarm systems, and evacuation plans to ensure full safety compliance.",
+      img: service1,
+    },
+    {
+      id: 2,
+      title: "Firefighting System Installation",
+      description: "Supply and install water, foam, and fire suppression systems tailored to your safety needs.",
+      img: service2,
+    },
+    {
+      id: 3,
+      title: "Advanced Fire Detection Systems",
+      description:
+        "Install addressable and conventional fire alarm devices, including smoke, heat, flame, and gas detectors.",
+      img: service3,
+    },
+    {
+      id: 4,
+      title: "Emergency Evacuation Systems",
+      description:
+        "Equip buildings with audio evacuation systems, fire-resistant walls, emergency doors, escape ladders, and assembly points.",
+      img: service4,
+    },
+    {
+      id: 5,
+      title: "Comprehensive Maintenance Services",
+      description:
+        "Ensure system reliability with routine and emergency maintenance for fire safety and prevention systems.",
+      img: service5,
+    },
+    {
+      id: 6,
+      title: "Fire Safety Training",
+      description: "Train clients on effective use of fire protection equipment and safety preventive measures.",
+      img: service6,
+    },
+    {
+      id: 7,
+      title: "Fire Safety Consultations",
+      description: "Provide expert consultations to optimize safety and fire protection systems for your facilities.",
+      img: service7,
+    },
+  ];
 
   return (
     <div className="container">
-        <h1 className="card-header">
-            Our services
-        </h1>
-        <Swiper
+      <h1 className="card-header">Our Services</h1>
+      <Swiper
         modules={[Navigation, Pagination, Scrollbar, A11y]}
         spaceBetween={20}
         slidesPerView={3}
         navigation
         pagination={{ clickable: true }}
-        scrollbar={{ draggable: true }}
+        scrollbar={{ draggable: true}}
         breakpoints={{
-          640: { slidesPerView: 1 }, // 1 slide for mobile screens
-          768: { slidesPerView: 2 }, // 2 slides for tablets
-          1024: { slidesPerView: 3 }, // 3 slides for desktops
+          640: { slidesPerView: 1 },
+          768: { slidesPerView: 2 },
+          1024: { slidesPerView: 3 },
         }}
       >
-        {cards.map((card) =>(
-            <SwiperSlide key={card.id}>
-                <div className="card">
-                    <img src={card.img} alt={cards.title}/>
-                    <div className="card-content">
-                        <h3>{card.title}</h3>
-                        <p>{card.description}</p>
-                    </div>
-                </div>
-            </SwiperSlide>
+        {cards.map((card) => (
+          <SwiperSlide key={card.id}>
+            <div className="card">
+              <img src={card.img} alt={card.title} />
+              <div className="card-content">
+                <h3>{card.title}</h3>
+                <p>{card.description}</p>
+              </div>
+            </div>
+          </SwiperSlide>
         ))}
       </Swiper>
     </div>
   );
 };
 
-export default CardSlider
+export default CardSlider;
