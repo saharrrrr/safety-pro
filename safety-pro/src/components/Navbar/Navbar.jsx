91570205e0c64744fa6b95b673be734a9f117c84
saharrrrr <skmao23@gmail.com>
2024-12-17 01:46:10 +0300

    update

--- safety-pro/src/components/Navbar/Navbar.jsx
+++ safety-pro/src/components/Navbar/Navbar.jsx
@@ -1,14 +1,33 @@
-import wlogo from '../../assets/white-logo.png'
-import logo from "../../assets/colored-logo.png"
-import "./Navbar.css"
+import React, { useState, useEffect } from "react";
+import wlogo from "../../assets/white-logo.png";
+import logo from "../../assets/colored-logo.png";
+import "./Navbar.css";
 
- const Navbar = () => {
+const Navbar = () => {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <div className='nav'>
-        <div className="nav-logo">
-            <img src={wlogo}/>
-        </div>
-        <ul className="nav-list">
+    <div className={`nav ${scrolled ? "scrolled" : ""}`}>
+      <div className="nav-logo">
+        <img src={scrolled ? logo : wlogo} alt="Logo" />
+      </div>
+      <ul className="nav-list">
         <li><a href="/">About us</a></li>
         <li><a href="/about">Services</a></li>
         <li><a href="/about">Projects</a></li>
@@ -17,7 +36,7 @@ import "./Navbar.css"
         <li><a href="/contact">Contact us</a></li>
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
