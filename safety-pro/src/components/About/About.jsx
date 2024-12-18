8ca4027108ec51ea4e2969439c2dc6ee96a984cc
saharrrrr <skmao23@gmail.com>
2024-12-18 16:33:16 +0300

    add about component

--- safety-pro/src/components/About/About.jsx
+++ safety-pro/src/components/About/About.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import logo from '../../assets/colored-logo.png'
+import './About.css'
+
 const About = () => {
   return (
     <div className='about-container'>
-        <h2 className='about-header'>About us</h2>
-        <div className="about-content">
-            <p className='content'>A leading fire safety and prevention company that provides comprehensive solutions for safety and fire protection. We specialize in delivering services and products that help our clients protect their property and lives.</p>
-            <img src= {logo} alt="logo" />
+        <div className="about-conent">
+          <h2 className='about-header'>About us</h2>
+          <div className="content">
+             <p >A leading fire safety and prevention company that provides comprehensive solutions for safety and fire protection. We specialize in delivering services and products that help our clients protect their property and lives.</p>
+             <img src= {logo} alt="logo" />
+          </div>
         </div>
     </div>
   )
