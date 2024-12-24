57abe8cb2c1ecc8daa026e943b516835f9888cd3
saharrrrr <skmao23@gmail.com>
2024-12-25 00:35:27 +0300

    make navbar responsive

--- safety-pro/src/components/Navbar/Navbar.jsx
+++ safety-pro/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
+import {FaBars, FaTimes} from "react-icons/fa";
 import wlogo from "../../assets/white-logo.png";
 import logo from "../../assets/colored-logo.png";
 import "./Navbar.css";
 
 const Navbar = () => {
   const [scrolled, setScrolled] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   const handleScroll = () => {
     const isScrolled = window.scrollY > 50;
@@ -21,12 +23,19 @@ const Navbar = () => {
     console.log("Scrolled state:", scrolled); // Debugging log
   }, [scrolled]);
 
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <div className={`nav ${scrolled ? "scrolled" : ""}`}>
       <div className="nav-logo">
         <img src={scrolled ? wlogo : logo} alt="Logo" />
       </div>
-      <ul className="nav-list">
+      <div className="nav-menu-icon" onClick={toggleMenu}>
+        {menuOpen ? <FaTimes /> : <FaBars />}
+      </div>
+      <ul className= {`nav-list ${menuOpen ? "open" : ""}`}>
         <li><a href="#about">About us</a></li>
         <li><a href="#services">Services</a></li>
         <li><a href="#projects">Clients</a></li>
